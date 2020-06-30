fa73f3ae403a22ba9759c7b0d9e371d9f70a63f5
cuong6218 <cuong6218@domain.com>
2020-06-30 11:01:33 +0700

    add file tach phuong thuc

---
+++ php-cleancode-refactoring-method-extraction/src/Cylinder.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: phanluan
+ * Date: 26/10/2018
+ * Time: 22:50
+ */
+class Cylinder
+{
+    public function getVolume($radius, $height)
+    {
+        
+        
+        $volume = $this->getPerimeter($radius) * $height + 2 * $this->getBaseArea($radius);
+        return $volume;
+    }
+    public function getBaseArea($radius){
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+    public function getPerimeter($radius){
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
+    }
+}
